e285574dbbd478aa692c780b0bbd218a86f08c03
Trushant Doke <trushant.doke@xebia.com>
2019-12-26 14:01:37 +0530

    prototype chaining

---
+++ packages/js-learnings/src/prototypes/inheritance.js
@@ -0,0 +1,7 @@
+/** 
+ * in js, if you want bunch of objects to share some common props and functions.
+ * create one constructor function and add common props and functions on its .prototype object
+ * all the objects created using constructor function will have access to these common props and functions through
+ * prototype chaining.
+ * 
+*/
